0d029dad9137853d0c356b8b22a7c312e6975cdd
irogov <irogoff88@gmail.com>
2017-02-12 10:42:51 +1000

    add structure

---
+++ chapter_001/src/test/java/ru/irogov/CalculatorTest.java
@@ -0,0 +1,21 @@
+package Calculator;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CalculatorTest
+{
+    @Test
+    public void whenSumReturnSum()
+    {
+         final Calculator calc = new Calculator();
+         calc.sum(1,1);
+         final double result = calc.getResult();
+         assertThat(result, is(2d));
+    }
+}
+
+Проанализировать RefTask. Будет выведено 2.
+Проанализировать ChangeState. Создан 1 объект. Будет выведено task.
+
+
